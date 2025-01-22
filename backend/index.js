702befc6a9479b004abd24acb648301202a58a17
raghammer1 <raghagarwal@gmail.com>
2025-01-22 16:26:29 +1100

    made mongo connection and initial server

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,48 @@
 const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+
+// PORT is for when app is hosted then API_PORT is for local environment
+const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 
-app.listen(port, () => {
-  console.log('app listening on port 3000');
+app.use(bodyParser.json({ limit: '500mb' }));
+
+app.use(express.json());
+app.use(cors());
+
+app.route('/').get((req, res) => {
+  res.send('Hi');
 });
+
+app.get('/proxy', (req, res) => {
+  const url = req.query.url;
+  if (!url) {
+    return res.status(400).send('URL is required');
+  }
+
+  request
+    .get(url)
+    .on('error', (err) => {
+      res.status(500).send(err.message);
+    })
+    .pipe(res);
+});
+
+const server = http.createServer(app);
+// socketServer.registerSocketServer(server);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log('Server running on port:', PORT);
+    });
+  })
+  .catch((err) => {
+    console.log('Database connection failed with error:', err);
+  });
